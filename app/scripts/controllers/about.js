30f79e23f2a89b07f394e3a7486cc3cff3c07dd9
rafaeldelbel <rafaeldbernardes@gmail.com>
2016-12-22 20:47:19 -0200

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name viajeAgoraApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the viajeAgoraApp
+ */
+angular.module('viajeAgoraApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
